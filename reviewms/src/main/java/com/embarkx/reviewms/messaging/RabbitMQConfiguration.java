c417314f510a8425c09cefa029659d97d6e2bf73
Alexinoo <mwangialex26@gmail.com>
2024-11-25 17:20:58 +0300

    Publishing Messages to RabbitMQ

---
+++ reviewms/src/main/java/com/embarkx/reviewms/messaging/RabbitMQConfiguration.java
@@ -0,0 +1,36 @@
+package com.embarkx.reviewms.messaging;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfiguration {
+
+
+    // Create a Queue
+    @Bean
+    public Queue companyRatingQueue(){
+        return new Queue("companyRatingQueue");
+    }
+
+
+    // Create a Converter to Serialize and Deserialize messages
+    @Bean
+    public MessageConverter jsonMessageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+    // Helper class to handle creation and release of resources when sending msgs to / or receiving msgs
+    // from RabbitMQ
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        return rabbitTemplate;
+    }
+}
---
+++ reviewms/src/main/java/com/embarkx/reviewms/messaging/ReviewMessageProducer.java
@@ -0,0 +1,24 @@
+package com.embarkx.reviewms.messaging;
+
+import com.embarkx.reviewms.dto.ReviewMessage;
+import com.embarkx.reviewms.model.Review;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
+public class ReviewMessageProducer {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public ReviewMessageProducer(RabbitTemplate rabbitTemplate){
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(Review newReview){
+        ReviewMessage reviewMessage = new ReviewMessage();
+        reviewMessage.setId(newReview.getId());
+        reviewMessage.setTitle(newReview.getTitle());
+        reviewMessage.setDescription(newReview.getDescription());
+        reviewMessage.setRating(newReview.getRating());
+        reviewMessage.setCompanyId(newReview.getCompanyId());
+
+    }
+}
